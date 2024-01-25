96555a308d0c6ade7dd2097523247a87f40216bd
achawayne3031 <59153600+achawayne3031@users.noreply.github.com>
2024-01-25 16:53:01 +0100

    update

--- src/utils/LocalStorage.ts
+++ src/utils/LocalStorage.ts
@@ -1,4 +1,6 @@
 import { reactLocalStorage } from 'reactjs-localstorage'
+import * as CryptoJS from 'crypto-js'
+import { ENCRYPTION_KEY } from './constantData'
 
 export const saveToken = (token: string) => {
   reactLocalStorage.set('@preyellc_token', token)
@@ -18,11 +20,9 @@ export const removeEventRef = () => {
 }
 
 export const saveUserData = (data: any) => {
-  reactLocalStorage.set('@preyellc_user', JSON.stringify(data))
-}
+  let encryptData = encryptCrytoJs(data, true)
 
-export const saveAdminData = (data: any) => {
-  reactLocalStorage.set('@preyellc_admin', JSON.stringify(data))
+  reactLocalStorage.set('@preyellc_user', encryptData)
 }
 
 export const getToken = () => {
@@ -32,8 +32,28 @@ export const getToken = () => {
 
 export const getUserData = () => {
   const storedUser: any = reactLocalStorage.get('@preyellc_user')
-  const jsonStored = storedUser ? JSON.parse(storedUser) : null
+  let deciperData = decipherCryptoJs(storedUser, true)
+
+  const jsonStored = storedUser ? deciperData : null
   return jsonStored
+}
+
+export const encryptCrytoJs = (data: any, json = false) => {
+  if (json) {
+    return CryptoJS.AES.encrypt(JSON.stringify(data), ENCRYPTION_KEY).toString()
+  }
+
+  return CryptoJS.AES.encrypt(data, ENCRYPTION_KEY).toString()
+}
+
+export const decipherCryptoJs = (data: any, json = false) => {
+  if (json) {
+    var bytes = CryptoJS.AES.decrypt(data, ENCRYPTION_KEY)
+    return JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
+  }
+
+  var bytes = CryptoJS.AES.decrypt(data, ENCRYPTION_KEY)
+  return bytes.toString(CryptoJS.enc.Utf8)
 }
 
 export const logUserOut = () => {
--- src/utils/constantData.ts
+++ src/utils/constantData.ts
@@ -1,1 +1,2 @@
 export const API_LOCAL_URL = 'http://localhost:3000/api'
+export const ENCRYPTION_KEY = 'preyellc'
